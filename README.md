79bfeaee257417c61472b75b103c36bfd585c095
kidcompassion <sallypoulsen@gmail.com>
2019-10-27 17:40:01 -0600

    Updated README and added pagination, search, and reset functionalities.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# treehouse_techdegree_project_two
-JS Pagination Project
+## JS Pagination Project
+
+This repo contains a single JavaScript page application that displays a list of students. It includes both pagination an search functionality.
+
+This project required DOM manipulation, writing functions, and breaking large tasks into smaller, more manageable pieces. It also requires code comments (which, BTW, I am always worried I write WAY too many of, so if that's the case, please feel free to say so :D) I also QA tested it in Chrome, Safari and Firefox (I don't have a PC or I would have done Edge as well).
+
+In addition to the main project, I also added search functionality, search pagination, and a reset button for users to return the listing to its default state after a search.
--- js/script.js
+++ js/script.js
@@ -2,70 +2,283 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+
+// Create variable containing global student list
+const list = document.getElementsByClassName('student-item');
+
+// By default, hide everything in that list
+for (var i = 0; i <= list.length -1; i++) {
+  list[i].style.display = 'none';
+}
+
+// By default, set listing to the first page of returns.
+let page = 0;
 
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-const list = 'list';
-let page = 1;
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+   Show Page - determine which students to show based on the selected page
 ***/
 
 const showPage = (list, page) => {
+  // Set how many records should appear on each page
+  const recordsPerPage = 10;
 
-  for (var i = 0; i <= 9; i +=1) {
-    console.log(i);
+  // If we're on the first page (data-attr 0), set the default start and endIndex
+  let startIndex = 0;
+  let endIndex = 9;
+
+  //if we're on a page greater than 1, run some math to get updated Start and end indexes
+  if(page >=1){
+      startIndex = (page * recordsPerPage);
+      endIndex = startIndex + (recordsPerPage-1)
+  } 
+
+  // Loop through the list (removing 1 to account for 0 start) and show/hide based on start and endIndexes
+  for (var i = 0; i <= list.length-1; i++) {
+    if(i >= startIndex && i <= endIndex){
+        list[i].style.display = 'block';
+    } else {
+      list[i].style.display = 'none';
+    }
   }
-
 }
 
-showPage();
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
 
-const appendPageLinks = () => {
-  //foreach group of ten students, add one value to page
-  const studentList = document.getElementsByClassName('student-item');
+const appendPageLinks = (list) => {
 
-  console.log(studentList);
+  // First, determine how many pages we need
+  // Get total number of students
+  let listLength = list.length;
 
-  //generate buttons
-  //append buttons to page
-  //add functionality to buttons
+  // Divide list by ten, and round it up to get the total number of pages
+  let noOfPages = Math.ceil(listLength/10);
 
-  //on click, run show page function, passing in which page it is, which will, in turndetermine what gets shown.
-  // on click, run the show page function based on which page button was clicked.
+  // Build the HTML elem with a link for each page
+  // Get the elem we want the pagination appended to
+  const listUl = document.getElementsByClassName('student-list');
 
+  // Create the new pagination elem
+  const createLinks = document.createElement('div');
+
+  // Set up an array to hold a link for each page number. 
+  const pageLink = [];
+
+  for (var i = 0; i < noOfPages; i++) {
+    pageLink[i] = `<a class="pagination" data-page=${i} href="#"> ${ i+1 } </a>`; 
+  }
+
+  //Give it a class
+  createLinks.className = 'listPagination';
+
+  // Flatten the array into page link markup
+  createLinks.innerHTML = pageLink.join('|');
+
+  // Add it to the bottom of the list
+  listUl[0].appendChild(createLinks);
+
+  // Grab all pagination links
+  const paginationLinks = document.querySelectorAll('.pagination');
+
+  //Add active class to the first pagination link
+  paginationLinks[0].classList.add('active');
+
+  // For each page, create a link and update the list
+  for ( var i = 0; i < paginationLinks.length; i++) {
+    paginationLinks[i].addEventListener('click', function(e){
+      e.preventDefault();
+      // Run function to remove all active classes to avoid repeats 
+      removeActiveClass(paginationLinks);
+      //Add active class to clicked element
+      this.classList.add('active');
+      // Get data-page value to dtermine which list to show
+      let pageLink = this.getAttribute('data-page');      
+      showPage(list, pageLink);
+    });
+  }
 }
 
-appendPageLinks();
+/***  
+  Remove all active classes to avoid duplication
+***/
+const removeActiveClass = () => {
+  const activeClasses = document.getElementsByClassName('active');
+  for ( var i = 0; i < activeClasses.length; i++) {
+    activeClasses[i].classList.remove('active');
+  }
+}
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+/*** 
+  Add Search functionality
+ ***/
+
+ // Set up markup for search box
+const generateSearch = () =>{
+  //Create wrapper div to hold search elem
+  const searchDiv = document.createElement('div');
+
+  // Create search field
+  const searchInput = document.createElement('input');
+  searchInput.setAttribute('type', 'text');
+  searchInput.setAttribute('placeholder', 'Search for students...');
+  searchDiv.className = 'student-search';
+  
+  // Create searchBtn 
+  const searchBtn = document.createElement('button');
+  searchBtn.innerHTML = 'Search';
+
+  //Append search Input and Button to search wrapper
+  searchDiv.appendChild(searchInput);
+  searchDiv.appendChild(searchBtn);
+
+  //append search wrapper and all children to page-header div in HTML
+  const pageHeader = document.querySelector('.page-header');
+  pageHeader.appendChild(searchDiv);
+
+  captureSearch(searchBtn, searchInput);
+}
+
+/***
+  Remove the default pagination so it can be replaced on search 
+***/
+const removePagination = ()=>{
+  // Grab the pagination div so I can replace it on search submission
+  const paginationDiv = document.getElementsByClassName('listPagination');
+
+  //Clear out the contents of the default pagination
+  paginationDiv[0].remove();
+}
+
+/***
+  Generate pagination based on number of search returns
+***/
+const searchPagination = (searchReturns) => {
+  removePagination();
+  // Generate new page links, calculated using search returns
+  appendPageLinks(searchReturns);
+  showPage(searchReturns, page);
+}
+
+/***
+  Capture search value
+***/
+const captureSearch = (searchBtn, searchInput) => {
+  
+  //bundle submission code for reuse in this function
+  searchSubmit =(e)=>{
+    //Before anything, check for an error message
+    const findErrorMsg = document.querySelector('.search-error'); 
+    
+    //If it exists, remove it
+    if(typeof(findErrorMsg)!= 'undefined' && findErrorMsg != null){
+      findErrorMsg.remove();
+    }
+
+    //Get search string and pass to the run search function
+    requestedString = searchInput.value;
+    runSearch(list, requestedString);
+    e.preventDefault();
+  }
+  
+  // Run submission code on click
+  searchBtn.addEventListener('click', function(e){
+    searchSubmit(e);
+  });
+
+  // Run submission code on enter
+  searchBtn.addEventListener("keyup", function(e) {
+    console.log('works?');
+    if (event.keyCode === 13) {
+      searchSubmit(e);
+    }
+  });
+}
+
+/***
+  Create and append a search error message
+***/
+const searchError = () =>{
+   const pageHeader = document.querySelector('.student-list');
+   const createErrorMsg = document.createElement('span');
+   createErrorMsg.className = 'search-error';
+   createErrorMsg.innerText = 'Sorry, No results. Please Try Again';
+   createErrorMsg.style.color = 'red';
+   pageHeader.appendChild(createErrorMsg);
+}
+
+/***
+  Run search for requested string
+***/
+const runSearch = (list, requestedString) =>{
+  let searchResults = [];
+  // Convert list collection into array
+  let listCollectionToArray = Array.prototype.slice.call( list );
+  // Filter the array for search terms.
+  let searchedList = listCollectionToArray.filter(function(obj){
+    // hide everything by default
+    obj.style.display = 'none';
+    // if object matches search, show it and add it to results array
+    if (obj.innerText.includes(requestedString)){
+      obj.style.display = 'block';
+      searchResults.push(obj);
+    }
+
+  });
+  
+  if(searchResults.length >0){
+    searchPagination(searchResults);
+  } else {
+    searchError();
+    searchPagination(0);
+  }
+}
+
+
+/***
+  Generate a button that allows the user to reset to default listings
+***/
+const generateResetBtn = ()=>{
+  // Create reset button
+  const resetBtn = document.createElement('button');
+  resetBtn.innerText = 'Reset Listing';
+  resetBtn.className = 'reset-listing'
+  const pageHeader = document.querySelector('.page-header');
+  pageHeader.appendChild(resetBtn);
+  resetListings();
+}
+
+/***
+  Add logic to reset to default listing
+***/
+const resetListings = () => {
+  const resetBtn = document.querySelector('.reset-listing');
+  // run submission code on click
+  resetBtn.addEventListener('click', function(e){
+    const list = document.getElementsByClassName('student-item');
+    showPage(list, page);
+    appendPageLinks(list);
+    removePagination();
+    e.preventDefault();
+  });
+
+  // run submission code on enter
+  resetBtn.addEventListener("keyup", function(e) {
+    if (event.keyCode === 13) {
+      const list = document.getElementsByClassName('student-item');
+      showPage(list, page);
+      removePagination();
+      appendPageLinks(list);
+      e.preventDefault();
+    }
+  });
+}
+
+generateResetBtn();
+showPage(list, page);
+appendPageLinks(list);
+generateSearch();
